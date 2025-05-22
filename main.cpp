a1d6a5ac4709bf91fa363b9c998dd8a29abb2d75
CoderNeo17 <cs23b050@smail.iitm.ac.in>
2025-05-22 22:13:28 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+#define ICACHE "./input/ICache.txt"
+#define DCACHE "./input/DCache.txt"
+#define REG_FILE "./input/RF.txt"
+#define OUTPUT "./output/output.txt"
+#define DCACHE_OUTPUT "./output/DCACHE.txt"
+
+#define ADD 0
+#define SUB 1
+#define MUL 2
+#define INC 3
+#define AND 4
+#define OR  5
+#define XOR 6
+#define NOT 7
+#define SLLI 8
+#define SRLI 9
+#define LI  10
+#define LD  11
+#define ST  12
+#define JMP 13
+#define BEQZ 14
+#define HLT 15
+
+int main(){
+    return 0;
+}
